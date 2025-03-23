7ae1236ce8e397716799e78ae438bee65d48bbaa
andriy_tok <andriy.tokovyy@gmail.com>
2025-03-23 12:51:57 +0200

    hw 1 added namespaces to avoid conflicts

--- src/hw_1/task_1/index.ts
+++ src/hw_1/task_1/index.ts
@@ -1,64 +1,68 @@
+namespace hw_1_task_1 {
+
 // #dYQNrBV
 // Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //     Вивести кожну змінну за допомогою: console.log
 
-let str_1: string = 'hello'
-let str_2: string = 'owu'
-let str_3: string = 'com'
-let d: string = 'ua'
-let num1: number = 1
-let num2: number = 10
-let num3: number = -999
-let num4: number = 123
-let pi: number = 3.14
-let float1: number = 2.7
-let num5: number = 16
-let bool1: boolean = true
-let bool2: boolean = false
+    let str_1: string = 'hello'
+    let str_2: string = 'owu'
+    let str_3: string = 'com'
+    let d: string = 'ua'
+    let num1: number = 1
+    let num2: number = 10
+    let num3: number = -999
+    let num4: number = 123
+    let pi: number = 3.14
+    let float1: number = 2.7
+    let num5: number = 16
+    let bool1: boolean = true
+    let bool2: boolean = false
 
-console.log(str_1);
-console.log(str_2);
-console.log(str_3);
-console.log(d);
-console.log(num1);
-console.log(num2);
-console.log(num3);
-console.log(num4);
-console.log(pi);
-console.log(float1);
-console.log(num5);
-console.log(bool1);
-console.log(bool2)
+    console.log(str_1);
+    console.log(str_2);
+    console.log(str_3);
+    console.log(d);
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+    console.log(num4);
+    console.log(pi);
+    console.log(float1);
+    console.log(num5);
+    console.log(bool1);
+    console.log(bool2)
 
 // #6Qb97gsv
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person
 // (Не об'єкт, просто за допомоги конкатенації)
 
-let firstName: string = 'Андрій'
-let lastName: string = 'Токовий'
-let middleName: string = 'Юрійович'
+    let firstName: string = 'Андрій'
+    let lastName: string = 'Токовий'
+    let middleName: string = 'Юрійович'
 // let person:string= firstName + ' ' + lastName + ' ' +middleName
-let person: string = `${firstName} ${lastName} ${middleName}`
-console.log(person);
+    let person: string = `${firstName} ${lastName} ${middleName}`
+    console.log(person);
 
 // #4N0y5tufA
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
-let a: number = 100;
-let b: string = '100';
-let c: boolean = true;
-console.log(typeof a, typeof b, typeof c)
+    let a: number = 100;
+    let b: string = '100';
+    let c: boolean = true;
+    console.log(typeof a, typeof b, typeof c)
 
 // #ruUtWDUI
 // Додаткове для тих хто цікавився prompt`oм
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
-let my_name: string | null = prompt('Введіть своє ім`я: ')
-let surname: string | null = prompt('Введіть своє прізвище: ')
-let middle_name: string | null = prompt('Введіть своє прізвище:')
+    let my_name: string | null = prompt('Введіть своє ім`я: ')
+    let surname: string | null = prompt('Введіть своє прізвище: ')
+    let middle_name: string | null = prompt('Введіть своє прізвище:')
 
-let full_name: string | null = `${my_name} ${surname} ${middle_name}`
+    let full_name: string | null = `${my_name} ${surname} ${middle_name}`
 
-if(typeof(my_name) === 'string' && typeof(surname) === 'string' && typeof(middle_name) === 'string'){
-    console.log(full_name)
-}else{
-    console.log('Дані було введено некоректно!')
+    if (typeof (my_name) === 'string' && typeof (surname) === 'string' && typeof (middle_name) === 'string') {
+        console.log(full_name)
+    } else {
+        console.log('Дані було введено некоректно!')
+    }
+
 }
--- src/hw_1/task_2/arrays.ts
+++ src/hw_1/task_2/arrays.ts
@@ -1,167 +1,172 @@
+namespace hw_1_task_2 {
+
 // -------------------------------Масиви та об'єкти-----------------------------
 // // #67kfznmiMl
 // // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
 
-let arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(...arr)
-console.log('-------------------------------')
+    let arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    console.log(...arr)
+    console.log('-------------------------------')
 
 // #sA3Gg1sCp
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" -
 // являється  масивом. Кожен автор має поля name та age.
-interface IBook {
-    title: string;
-    pageCount: number;
-    genre: string;
-    authors: { name: string, age: number }[]
-}
-
-let books:IBook[] = [
-    {
-        title: 'Lord of the Rings',
-        pageCount: 800,
-        genre: 'fantazy',
-        authors: [
-            {name: 'J.R.Tolkien', age: 80},
-        ]
-    },
-    {
-        title: 'The Great Gatsby',
-        pageCount: 200,
-        genre: 'adventure',
-        authors: [
-            {name: 'author_1', age: 50},
-            {name: 'author_2', age: 40},
-        ],
-    },
-    {
-        title: 'Sherlok Homes',
-        pageCount: 300,
-        genre: 'detective',
-        authors: [
-            {name: 'Konan Doyle', age: 75}
-        ]
+    interface IBook {
+        title: string;
+        pageCount: number;
+        genre: string;
+        authors: { name: string, age: number }[]
     }
-]
 
-console.log(books)
-console.log('-------------------------------')
+    let books: IBook[] = [
+        {
+            title: 'Lord of the Rings',
+            pageCount: 800,
+            genre: 'fantazy',
+            authors: [
+                {name: 'J.R.Tolkien', age: 80},
+            ]
+        },
+        {
+            title: 'The Great Gatsby',
+            pageCount: 200,
+            genre: 'adventure',
+            authors: [
+                {name: 'author_1', age: 50},
+                {name: 'author_2', age: 40},
+            ],
+        },
+        {
+            title: 'Sherlok Homes',
+            pageCount: 300,
+            genre: 'detective',
+            authors: [
+                {name: 'Konan Doyle', age: 75}
+            ]
+        }
+    ]
+
+    console.log(books)
+    console.log('-------------------------------')
 
 // #jCHFnEbdmFd
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password.
 // Вивести в консоль пароль кожного користувача
-interface IUser{
-    name:string,
-    username:string,
-    password:string,
-}
-
-let users:IUser[] = [
-    {
-        name: "user_1",
-        username: "Andrusha_ua",
-        password: "Andrik007",
-    },
-    {
-        name: "user_2",
-        username: "ShadowWolf92",
-        password: "Pass1234!",
-    },
-    {
-        name: "user_3",
-        username: "CyberNinja",
-        password: "Hacker_007",
-    },
-    {
-        name: "user_4",
-        username: "PixelStorm",
-        password: "qwerty2024",
-    },
-    {
-        name: "user_5",
-        username: "QuantumByte",
-        password: "Xyz@9876",
-    },
-    {
-        name: "user_6",
-        username: "NeonTiger",
-        password: "SecurePass99",
-    },
-    {
-        name: "user_7",
-        username: "GhostRider",
-        password: "LetMeIn!",
-    },
-    {
-        name: "user_8",
-        username: "DarkPhoenix",
-        password: "P@ssw0rd",
-    },
-    {
-        name: "user_9",
-        username: "SkyWalker",
-        password: "StarWars123",
-    },
-    {
-        name: "user_10",
-        username: "CodeMaster",
-        password: "DebugMeNow",
+    interface IUser {
+        name: string,
+        username: string,
+        password: string,
     }
 
-];
+    let users: IUser[] = [
+        {
+            name: "user_1",
+            username: "Andrusha_ua",
+            password: "Andrik007",
+        },
+        {
+            name: "user_2",
+            username: "ShadowWolf92",
+            password: "Pass1234!",
+        },
+        {
+            name: "user_3",
+            username: "CyberNinja",
+            password: "Hacker_007",
+        },
+        {
+            name: "user_4",
+            username: "PixelStorm",
+            password: "qwerty2024",
+        },
+        {
+            name: "user_5",
+            username: "QuantumByte",
+            password: "Xyz@9876",
+        },
+        {
+            name: "user_6",
+            username: "NeonTiger",
+            password: "SecurePass99",
+        },
+        {
+            name: "user_7",
+            username: "GhostRider",
+            password: "LetMeIn!",
+        },
+        {
+            name: "user_8",
+            username: "DarkPhoenix",
+            password: "P@ssw0rd",
+        },
+        {
+            name: "user_9",
+            username: "SkyWalker",
+            password: "StarWars123",
+        },
+        {
+            name: "user_10",
+            username: "CodeMaster",
+            password: "DebugMeNow",
+        }
 
-console.log('Passwords of users:')
-for (const user of users){
-    console.log(user.password);
-}
+    ];
 
-console.log('--------------------------------')
+    console.log('Passwords of users:')
+    for (const user of users) {
+        console.log(user.password);
+    }
+
+    console.log('--------------------------------')
 
 // #coYydZuaeEB
 // - описати масив, в якому буде зберігатись інформація про температуру вранці, вдень і ввечері за термін в 7 днів.
 //     Як зробити цей масив - вам потрібно подумати. Нормальних варіантів опису - 2. Варіант, коли в вас буде
 // одновимірний масив з 21 значенням вичключаємо одразу
 
-interface ITemperature{
-    morning:number;
-    afternoon:number;
-    evening:number;
-}
+    interface ITemperature {
+        morning: number;
+        afternoon: number;
+        evening: number;
+    }
 
-let temperatures:ITemperature[] = [
-    {
-        morning: -2,
-        afternoon: 1,
-        evening: -1,
-    },
-    {
-        morning: 0,
-        afternoon: 3,
-        evening: -2,
-    },
-    {
-        morning: -3,
-        afternoon: 4,
-        evening: -1,
-    },
-    {
-        morning: -5,
-        afternoon: 2,
-        evening: -4,
-    },
-    {
-        morning: 1,
-        afternoon: 5,
-        evening: 0,
-    },
-    {
-        morning: -1,
-        afternoon: 6,
-        evening: -3,
-    },
-    {
-        morning: -4,
-        afternoon: 2,
-        evening: -2,
-    },
-];
+    let temperatures: ITemperature[] = [
+        {
+            morning: -2,
+            afternoon: 1,
+            evening: -1,
+        },
+        {
+            morning: 0,
+            afternoon: 3,
+            evening: -2,
+        },
+        {
+            morning: -3,
+            afternoon: 4,
+            evening: -1,
+        },
+        {
+            morning: -5,
+            afternoon: 2,
+            evening: -4,
+        },
+        {
+            morning: 1,
+            afternoon: 5,
+            evening: 0,
+        },
+        {
+            morning: -1,
+            afternoon: 6,
+            evening: -3,
+        },
+        {
+            morning: -4,
+            afternoon: 2,
+            evening: -2,
+        },
+    ];
+
+
+}
